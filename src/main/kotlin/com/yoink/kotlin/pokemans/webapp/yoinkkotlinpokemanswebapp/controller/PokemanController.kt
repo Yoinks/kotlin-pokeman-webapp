2484ce71392756106c84599d728c18b6c41a2037
Cody D Walling <codyDwalling@gmail.com>
2021-01-14 15:18:15 -0700

    removed rowmapper

--- src/main/kotlin/com/yoink/kotlin/pokemans/webapp/yoinkkotlinpokemanswebapp/controller/PokemanController.kt
+++ src/main/kotlin/com/yoink/kotlin/pokemans/webapp/yoinkkotlinpokemanswebapp/controller/PokemanController.kt
@@ -1,20 +1,11 @@
 package com.yoink.kotlin.pokemans.webapp.yoinkkotlinpokemanswebapp.controller
 
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.RowMapper
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.sql.ResultSet
 
 @RestController
 class PokemanController (val jdbcTemplate: JdbcTemplate) {
-
-    var PokemansRowMapper: RowMapper<Pokemans> = RowMapper<Pokemans> { resultSet: ResultSet, rowIndex: Int ->
-        Pokemans(
-                resultSet.getInt("id"),
-                resultSet.getString("name"),
-                resultSet.getString("type"))
-    }
 
     @GetMapping("/pokemans")
     fun getPokemans() = jdbcTemplate.query("SELECT * FROM pokeman", PokemansRowMapper)
--- src/main/kotlin/com/yoink/kotlin/pokemans/webapp/yoinkkotlinpokemanswebapp/controller/Pokemans.kt
+++ src/main/kotlin/com/yoink/kotlin/pokemans/webapp/yoinkkotlinpokemanswebapp/controller/Pokemans.kt
@@ -1,3 +1,13 @@
 package com.yoink.kotlin.pokemans.webapp.yoinkkotlinpokemanswebapp.controller
 
+import org.springframework.jdbc.core.RowMapper
+import java.sql.ResultSet
+
 data class Pokemans(val id: Int, val name: String, val type: String)
+
+var PokemansRowMapper: RowMapper<Pokemans> = RowMapper<Pokemans> { resultSet: ResultSet, rowIndex: Int ->
+    Pokemans(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("type"))
+}
